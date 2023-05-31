d20ca924f96e2857e96b18a8bb154a89b8d4721e
Filip-Laptop <filip.mochon@gmail.com>
2023-05-31 19:25:03 +0200

    poprawiony wygląd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_082908) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_100325) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_082908) do
     t.integer "stopien_postepu"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_082908) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "todos", "users"
 end
